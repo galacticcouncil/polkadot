d9f32e0c6f8f568e3fb21f20d951fd1bb6572d8c
martinfridrich <mrtnfridrich@gmail.com>
2021-08-05 10:47:20 +0200

    hydra-dx testnet setup

--- scripts/prepare-test-net.sh
+++ scripts/prepare-test-net.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$#" -ne 1 ]; then
-	echo "Please provide the number of initial validators!"
-	exit 1
-fi
-
 generate_account_id() {
-	subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "Account ID" | awk '{ print $3 }'
+	../substrate/target/release/subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "Account ID" | awk '{ print $3 }'
 }
 
 generate_address() {
-	subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "SS58 Address" | awk '{ print $3 }'
+	../substrate/target/release/subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "SS58 Address" | awk '{ print $3 }'
 }
 
 generate_public_key() {
-	subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "Public" | awk '{ print $4 }'
+	../substrate/target/release/subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "Public" | awk '{ print $4 }'
+}
+
+generate_secret_seed() {
+	../substrate/target/release/subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "Secret seed" | awk '{ print $3 }'
 }
 
 generate_address_and_public_key() {
@@ -37,11 +36,18 @@ generate_address_and_account_id() {
 	printf "//$ADDRESS\nhex![\"${ACCOUNT#'0x'}\"].$INTO(),"
 }
 
-V_NUM=$1
+generate_secret_seed_for() {
+ 	SEED=$(generate_secret_seed $1 $2 $3)
+  
+	printf "$2: $SEED"
+}
+
+S_NUM=$1
+E_NUM=$(($1+$2 - 1))
 
 AUTHORITIES=""
 
-for i in $(seq 1 $V_NUM); do
+for i in $(seq $S_NUM $E_NUM); do
 	AUTHORITIES+="(\n"
 	AUTHORITIES+="$(generate_address_and_account_id $i stash)\n"
 	AUTHORITIES+="$(generate_address_and_account_id $i controller)\n"
@@ -51,8 +57,21 @@ for i in $(seq 1 $V_NUM); do
 	AUTHORITIES+="$(generate_address_and_account_id $i para_validator '--scheme sr25519' true)\n"
 	AUTHORITIES+="$(generate_address_and_account_id $i para_assignment '--scheme sr25519' true)\n"
 	AUTHORITIES+="$(generate_address_and_account_id $i authority_discovery '--scheme sr25519' true)\n"
-	AUTHORITIES+="$(generate_address_and_public_key $i beefy '--scheme ecdsa' true)\n"
 	AUTHORITIES+="),\n"
 done
 
 printf "$AUTHORITIES"
+
+SECRETS=""
+for j in $(seq $S_NUM $E_NUM); do
+	SECRETS+="(\n"
+	SECRETS+="$(generate_secret_seed_for $j babe '--scheme sr25519' true)\n"
+	SECRETS+="$(generate_secret_seed_for $j grandpa '--scheme ed25519' true)\n"
+	SECRETS+="$(generate_secret_seed_for $j im_online '--scheme sr25519' true)\n"
+	SECRETS+="$(generate_secret_seed_for $j para_validator '--scheme sr25519' true)\n"
+	SECRETS+="$(generate_secret_seed_for $j para_assignment '--scheme sr25519' true)\n"
+	SECRETS+="$(generate_secret_seed_for $j authority_discovery '--scheme sr25519' true)\n"
+	SECRETS+="),\n"
+done
+
+printf "$SECRETS"
